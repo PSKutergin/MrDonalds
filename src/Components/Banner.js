345f74609d0b1e2826e25de1e21bbb7c0e48723c
PSKutergin <PSKutergin@gmail.com>
2023-12-06 14:03:19 +0300

    add banner

---
+++ src/Components/Banner.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import bannerImg from '../images/banner.png'
+
+export const Banner = styled.div`
+    width: 100%;
+    height: 200px;
+    background: url(${bannerImg}) center;
+    background-size: cover;
+`;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import dbMenu from './DBMenu'
 import { ListItem } from './ListItem';
+import { Banner } from './Banner';
 
 const MenuStyled = styled.main`
     background-color: #ccc;
@@ -14,6 +15,7 @@ const SectionMenu = styled.section`
 
 export const Menu = () => (
     <MenuStyled>
+        <Banner />
         <SectionMenu>
             <h2>Бургеры</h2>
             <ListItem itemList={dbMenu.burger} />
